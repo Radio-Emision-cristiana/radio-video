8148c28d25e04fd9c6fd500ae56b613520e7489a
Radio-Emision-cristiana <radioemisioncristiana@gmail.com>
2026-03-05 21:15:54 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+echo "INICIANDO FFMPEG..."
+
 mkdir -p public/hls
 
-ffmpeg -re-stream_loop -1 -i video.mp4 -i https://stream.zeno.fm/yg7bvksbfwzuv \
+ffmpeg -re -stream_loop -1 -i video.mp4 \
+-i https://stream.zeno.fm/yg7bvksbfwzuv \
 -c:v libx264 -preset veryfast \
 -c:a aac -b:a 128k \
 -f hls \
 -hls_time 4 \
 -hls_list_size 6 \
 -hls_flags delete_segments \
-
 public/hls/stream.m3u8
-
