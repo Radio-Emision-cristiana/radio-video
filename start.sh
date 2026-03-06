1cc574d45f44eb362c91ccbd479f620210bf702b
Radio-Emision-cristiana <radioemisioncristiana@gmail.com>
2026-03-05 21:57:19 -0400

    Update FFMPEG command to include audio mapping

--- start.sh
+++ start.sh
@@ -6,8 +6,10 @@ mkdir -p public/hls
 
 ffmpeg -re -stream_loop -1 -i video.mp4 \
 -i https://stream.zeno.fm/yg7bvksbfwzuv \
--c:v libx264 -preset veryfast \
+-map 0:v:0 -map 1:a:0 \
+-c:v libx264 -preset veryfast -pix_fmt yuv420p \
 -c:a aac -b:a 128k \
+-shortest \
 -f hls \
 -hls_time 4 \
 -hls_list_size 6 \
