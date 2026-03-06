77e3ac9cb5df8045179aac884d1c3aeb756346a2
Radio-Emision-cristiana <radioemisioncristiana@gmail.com>
2026-03-05 23:09:55 -0400

    Modify start.sh for ffmpeg streaming parameters
    Updated ffmpeg command options for improved streaming.

--- start.sh
+++ start.sh
@@ -5,18 +5,18 @@ echo "INICIANDO FFMPEG..."
 mkdir -p public/hls
 
 ffmpeg -re -stream_loop -1 -i video.mp4 \
--reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
--i https://stream.zeno.fm/yg7bvksbfwzuv \
+-thread_queue_size 512 -i https://stream.zeno.fm/yg7bvksbfwzuv \
 -map 0:v:0 -map 1:a:0 \
 -c:v libx264 -preset veryfast \
 -vf scale=854:480 \
 -pix_fmt yuv420p \
 -c:a aac -b:a 128k \
--af aresample=async=1 \
+-af aresample=async=1000 \
 -f hls \
 -hls_time 4 \
 -hls_list_size 6 \
--hls_flags delete_segments+append_list \
+-hls_flags delete_segments \
 public/hls/stream.m3u8
+
 
 
